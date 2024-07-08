6f6b53118421f5ced2b21752b78f582c82300dda
Rift <hegide.dev@gmail.com>
2024-07-08 16:48:47 -0400

    fixed btn

--- qr-code-generator/src/App.jsx
+++ qr-code-generator/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import QRCode from "qrcode.react";
 
 function Form() {
+  const [url2, setUrl2] = useState("");
   const [url, setUrl] = useState("");
   function handleSubmit(e) {
     e.preventDefault();
+    setUrl2(url);
   }
   return (
     <div id="qr-code">
-      <QRCode value={url} />
+      <QRCode value={url2} />
       <form method="post" onSubmit={handleSubmit}>
         <input
           type="url"
