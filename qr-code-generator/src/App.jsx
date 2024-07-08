bf9bc41e667577214cd24cb467d1a174bd971d2b
Rift <hegide.dev@gmail.com>
2024-07-08 12:06:21 -0400

    add qrcode

--- qr-code-generator/src/App.jsx
+++ qr-code-generator/src/App.jsx
@@ -1,31 +1,37 @@
 import { useState } from "react";
+import QRCode from "qrcode.react";
+
 function Form() {
   const [url, setUrl] = useState("");
   function handleSubmit(e) {
     e.preventDefault();
   }
   return (
-    <form method="post" onSubmit={handleSubmit}>
-      <input
-        type="url"
-        placeholder="url"
-        value={url}
-        onChange={(e) => setUrl(e.target.value)}
-      />
-      <button type="submit" className="btn-generate-code">
-        Gererate
-      </button>
-    </form>
+    <div id="qr-code">
+      <QRCode value={url} />
+      <form method="post" onSubmit={handleSubmit}>
+        <input
+          type="url"
+          placeholder="url"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <button type="submit" className="btn-generate-code">
+          Gererate
+        </button>
+      </form>
+    </div>
   );
 }
 
 function Card() {
   return (
-    <div className="card">
-      <h1>Generate QR code</h1>
-      <div id="qr-code"></div>
-      <Form />
-    </div>
+    <>
+      <div className="card">
+        <h1>Generate QR code</h1>
+        <Form />
+      </div>
+    </>
   );
 }
 
@@ -33,7 +39,6 @@ export default function App() {
   return (
     <>
       <Card />
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
     </>
   );
 }
