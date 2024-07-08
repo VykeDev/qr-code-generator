f639dc1e7859e83772ebb358b08df83041b20cd8
Rift <hegide.dev@gmail.com>
2024-07-08 11:27:41 -0400

    form component

--- qr-code-generator/src/App.jsx
+++ qr-code-generator/src/App.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
+function Form() {
+  const [url, setUrl] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+  return (
+    <form method="post" onSubmit={handleSubmit}>
+      <input
+        type="url"
+        placeholder="url"
+        value={url}
+        onChange={(e) => setUrl(e.target.value)}
+      />
+      <button type="submit" className="btn-generate-code">
+        Gererate
+      </button>
+    </form>
+  );
+}
+
 function Card() {
   return (
     <div className="card">
       <h1>Generate QR code</h1>
-      <div className="qr-code"></div>
-      <form>
-        <input type="url" name="url" id="url" placeholder="url" />
-        <button className="btn-generate-code">Gererate</button>
-      </form>
+      <div id="qr-code"></div>
+      <Form />
     </div>
   );
 }
@@ -15,6 +33,7 @@ export default function App() {
   return (
     <>
       <Card />
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/qrcodejs/1.0.0/qrcode.min.js"></script>
     </>
   );
 }
